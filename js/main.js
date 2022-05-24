d47e41550666ed8bf09e5c4f4e0a97bb81682275
Tristan Gottschalk <trstn@gttschlk.com>
2022-05-24 11:37:38 -0400

    add text change on conversion select / refractor repeated code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-let annualSalary
-let hourlyRate
+// hide output text until user selects a conversion type
+document.querySelector("#userInputs").style.display = "none"
+document.querySelector("#userInput").value = ''
+
 const dropdownList = document.querySelector('#dropdownList')
+const userInput = document.querySelector('#userInput').value
+
+
 
 dropdownList.addEventListener("change",(e)=>{
     // assign value from dropdownList 
     const value = e.target.value
-
+    const conversionType = document.querySelector("#conversionType")
+    const converstionText = document.querySelector("#conversionText")
+    // display user text on change
+    document.querySelector("#userInputs").style.display = "block"
     // change text based on which dropdown option is selected
     if (value == "hourToAnnual"){
-        document.querySelector("#conversionType").textContent = "hourToAnnual"
+        conversionType.textContent = "hourly rate"
+        converstionText.textContent = 
+        "annual salary"
     }else if (value == "annualToHour"){
-         document.querySelector("#conversionType").textContent = "annualToHour"
-     }else {
-        document.querySelector("#conversionType").textContent = "none selected"
-    }
+         conversionType.textContent = "annual salary"
+         converstionText.textContent = 
+         "hourly rate"
+     }
 })
 
 
@@ -21,86 +31,13 @@ dropdownList.addEventListener("change",(e)=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let userCalc = document.querySelector('#dropdownList').value
-// document.querySelector('#enter').addEventListener('click', changeCalc)
-
-// changes event listener function based on dropdown selected option
-// function changeCalc(){
-//     if (userCalc == 'hourToAnnual'){
-//         hourlyToAnnualConversion()
-//     } else if (userCalc == 'annualToHour'){
-//         annualToHourlyConversion()
-//     }
-// }
-
-// changes text before user input depending on dropdown
-// let conversionType = document.querySelector('#conversionType')
-// document.querySelector('#dropdownList').onchange = handleChange
-// function handleChange(e){
-//     if (userCalc == 'hourToAnnual'){
-//         conversionType.textContent = 'hourly rate'
-//         console.log('changed to hourToAnnual')
-//     } else if (userCalc == 'annualToHour'){
-//         conversionType.textContent = 'annual salary'
-//         console.log('changed to annualToHour')
-//     }
-// }
-
 // converts hourly rate to annual salary
-// function hourlyToAnnualConversion(){
-//     hourlyRate = document.querySelector('#userInput').value
-//     annualSalary = hourlyRate * 52 * 40
-//     document.querySelector('#output').innerText = `$${annualSalary.toLocaleString("en-US")}`
-// }
+function hourlyToAnnualConversion(userInput){
+    annualSalary = userInput * 52 * 40
+    document.querySelector('#output').innerText = `$${annualSalary.toLocaleString("en-US")}`
+}
 // convert annual salary to hourly rate
-// function annualToHourlyConversion(){
-//     annualSalary = document.querySelector('#userInput').value
-//     hourlyRate = annualSalary / 52 / 40
-//     document.querySelector('#output').innerText = `$${hourlyRate.toLocaleString("en-US")}`
-// }
+function annualToHourlyConversion(userInput){
+    hourlyRate = userInput / 52 / 40
+    document.querySelector('#output').innerText = `$${hourlyRate.toLocaleString("en-US")}`
+}
